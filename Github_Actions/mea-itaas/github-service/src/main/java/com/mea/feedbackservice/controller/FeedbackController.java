0fc980667d599e7aa98c96ad6db0f096b0c605fe
PavankumarSP13 <pavankumar.sp@triconinfotech.com>
2024-07-29 10:59:32 +0530

    initial commit

---
+++ Github_Actions/mea-itaas/github-service/src/main/java/com/mea/feedbackservice/controller/FeedbackController.java
@@ -0,0 +1,17 @@
+package com.mea.feedbackservice.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/feedback-service")
+public class FeedbackController {
+    @GetMapping("/test")
+    public ResponseEntity<String> test(HttpServletRequest request) {
+        return new ResponseEntity<String>("Feedback Service!!",HttpStatus.OK);
+    }
+}
---
+++ Github_Actions/mea-itaas/github-service/src/test/java/com/mea/feedbackservice/FeedbackApplicationTests.java
@@ -0,0 +1,18 @@
+package com.mea.feedbackservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by premsingh on 6/5/18.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeedbackApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
