fb7e4eca93b762827c8f048629c516ccbca9e10f
PavankumarSP13 <pavankumar.sp@triconinfotech.com>
2024-08-02 17:15:28 +0530

    dir deletec

--- Github_Actions/mea-itaas/api-gateway/src/main/java/com/mea/apigateway/repository/AppProperyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mea.apigateway.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.mea.apigateway.model.AppProperty;
-
-public interface AppProperyRepository extends MongoRepository<AppProperty,String>{
-
-}
