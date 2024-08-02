cbcad905214572a0501e9fc1c4e06967243c2f5d
PavankumarSP13 <pavankumar.sp@triconinfotech.com>
2024-08-02 16:41:42 +0530

    controller added

---
+++ Github_Actions/mea-itaas/api-gateway/src/main/java/com/mea/apigateway/controller/api-gateway-controller.java
@@ -0,0 +1,18 @@
+package com.mea.feedbackservice.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/feedback-service")
+public class FeedbackController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test(HttpServletRequest request) {
+        return new ResponseEntity<String>("API-Gateway Dev Service is UP & CICD implemented!!!",HttpStatus.OK);
+    }
+}
